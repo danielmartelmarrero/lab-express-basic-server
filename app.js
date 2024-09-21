06b9f771128598532cf622716eac3620a270e264
Daniel Martel <danielmartelmarrero@gmail.com>
2024-09-21 13:01:46 +0100

    .

--- app.js
+++ app.js
@@ -23,3 +23,36 @@
 
 // START THE SERVER
 // Make your Express server listen on port 5005:
+
+
+const express = require('express')
+const morgan  = require('morgan')
+
+const app = express()
+app.listen(5005)
+
+app.use(express.static('public'))       //Tells that all the images are stored in the public folder
+app.use(express.json())
+app.use(morgan('dev'))
+
+const projects = 
+
+app.get('/', (req, res)=>{
+    res.sendFile(__dirname + '/views/home.html')
+})
+app.get('/blog', (req, res)=>{
+    res.sendFile(__dirname + '/views/blog.html')
+})
+app.get('/not-found', (req, res)=>{
+    res.sendFile(__dirname + '/views/not-found.html')
+})
+
+
+
+app.get('/api/projects',(req,res)=>{
+    res.sendFile(__dirname+'/data/projects.json')
+})
+
+app.get('/api/articles',(req,res)=>{
+    res.sendFile(__dirname+'/data/articles.json')
+})
